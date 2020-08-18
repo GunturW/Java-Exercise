f61adb1dfebbfd53c8e314ddd8969513a7eb154d
Guntur Wicaksono <gunturw79@gmail.com>
2020-08-18 19:01:22 +0700

    Soal GG SM

---
+++ src/exercise/java/ExerciseJava.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exercise.java;
+
+/**
+ *
+ * @author 247
+ */
+import java.util.Arrays;
+public class ExerciseJava {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int[] komposit = {4,6,8,9,10,12,14,15,16,18};
+        for(int i = 0; i <= (komposit.length-1); i++){
+            if(i % 2 == 0){
+                System.out.print(komposit[i]+" ");
+                
+            }
+           
+            
+        }
+       
+    }
+    
+}
